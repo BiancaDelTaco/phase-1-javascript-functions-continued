062a8d28cd6d040639fdbfc6de9df3f4e69476b3
Nicole Gage <nicole.m.gage@gmail.com>
2021-09-28 20:50:48 +0000

    commit tree 10e659182241998e012bd7a2786804b113361781

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+
+function saturdayFun (activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+
+function mondayWork(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+let wrapAdjective = function(style="*") {
+    return function (adjective="hard") {
+        return (`You are ${style}${adjective}${style}!`) 
+        
+    }
+}
